fa3e3ae0613097c36ace06cdda8dca6d4ee7c710
kshitij879 <kshitij.gupta_cs18@gla.ac.in>
2019-10-05 23:21:35 +0530

    que6 completed

---
+++ src/solution/Que6.java
@@ -0,0 +1,12 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Que6 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine().trim();
+        String str1=str.toUpperCase();
+        System.out.println(str1);
+    }
+}
